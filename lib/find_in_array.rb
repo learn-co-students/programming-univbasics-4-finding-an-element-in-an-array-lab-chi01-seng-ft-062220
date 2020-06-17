8c68d16d02902e1056e3ea4b5d0c169776cb5094
Ladeeluc <laura.hatch.lh@gmail.com>
2020-06-17 12:09:13 -0500

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
+# def find_element_index(array, value_to_find)
+#   array.index(value_to_find)
+# end
+
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+i = 0
+  while i < array.length  do
+    if array[i] == value_to_find
+    return i
+  end
+  i += 1
+  end
 end
+
